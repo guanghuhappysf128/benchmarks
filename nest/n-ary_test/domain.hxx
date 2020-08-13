3c9abec7eded6d1bc29d3a2880a96538e7f851d0
guanghuhappysf128 <ghu1@student.unimelb.edu.au>
2020-08-13 10:55:35 +1000

    Add n-ary test cases

---
+++ nest/n-ary_test/domain.hxx
@@ -0,0 +1,121 @@
+//
+// Created by ghu1 on 21/08/18.
+//
+#include "epistemic_checker.hxx"
+
+#ifndef SIM_GEN_DOMAIN_H
+#define SIM_GEN_DOMAIN_H
+
+using namespace std;
+
+bool Agent::seesAgent(Agent &target)
+{
+
+    string agent_l,target_l;
+    for (auto i: this->variables)
+    {
+        if (i.sameName("location")) agent_l = i.getValue();
+    }
+    for (auto i: target.variables)
+    {
+        if (i.sameName("location")) target_l = i.getValue();
+    }
+    return sees(agent_l,target_l);
+};
+
+bool Agent::seesObject(Object &target)
+{
+
+    string agent_l,target_l;
+    for (auto i: this->variables)
+    {
+        if (i.sameName("location")) agent_l = i.getValue();
+    }
+    for (auto i: target.variables)
+    {
+        if (i.sameName("location")) target_l = i.getValue();
+    }
+    return sees(agent_l,target_l);
+};
+
+Agent Agent::seesAV(Agent &a)
+{
+    string agent_id=a.getId();
+    Agent agent(agent_id);
+    for (auto i: a.variables)
+    {
+        if (i.sameName("location")) agent.addVariable(i.getName(),i.getValue());
+        else if (this->seesAgent(a)) agent.addVariable(i.getName(),i.getValue());
+    }
+    return agent;
+};
+
+Object Agent::seesOV(Object &o)
+{
+    string agent_id=o.getId();
+    Object object(agent_id);
+    for (auto i: o.variables)
+    {
+        if (i.sameName("location")) object.addVariable(i.getName(),i.getValue());
+        else if (this->seesObject(o)) object.addVariable(i.getName(),i.getValue());
+    }
+    return object;
+};
+
+bool ProblemState::checkS(Seeing &s)
+{
+    // in this domain sees means knowing whether
+    // for nested seeing relationship
+    // seeing means sees all direct first children agent
+    // and knowing their child's location
+    // since the assumption is location is aware by all agents
+    // only need to check sees children
+    // and the prove for sees that is the agent has non-empty
+    //  range or direction
+
+    //if the next sees is a variable, which mean we want to check whether
+    if (s.seeing_ptr->ptr_type == Seeing::VARIABLE&&s.seeing_ptr->seeing_type==Seeing::SV)
+    {
+        for (auto i:s.agent_ids)
+        {
+            if (this->findAgent(i))
+            {
+                Agent a = this->getAgent(i);
+                if (this->findAgent(s.seeing_ptr->variable_ptr->getId()))
+                {
+                    Agent target = this->getAgent(s.seeing_ptr->variable_ptr->getId());
+                    return a.seesAgent(target);
+                }
+                else if (this->findObject(s.seeing_ptr->variable_ptr->getId()))
+                {
+                    Object target = this->getObject(s.seeing_ptr->variable_ptr->getId());
+                    return a.seesObject(target);
+                } else return false;
+            }
+        }
+    }
+
+    for (auto i:s.seeing_ptr->agent_ids)
+    {
+        if (this->findAgent(i))
+        {
+            //this need to be a feature that every agent have, and it cannot be location because everyone know others location as assumption
+            if (this->getAgent(i).getV("secret")=="NONE")
+                return false;
+        } else return false;
+
+
+    }
+    return true;
+}
+
+//might be need to be changed, using int for now
+bool sees(string agent_l,string target_l)
+{
+
+    int agent = stoi(agent_l);
+    int target = stoi(target_l);
+    return abs(agent-target)<=1;
+};
+
+#endif //SIM_GEN_DOMAIN_H
---
+++ nest/n-ary_test/domain.pddl
@@ -0,0 +1,100 @@
+(define (domain corridor)
+	(:requirements :strips :typing :equality :adl)
+
+	(:types 
+		pos neg - query
+		loc boolean agent - int
+		room 
+	)
+
+	(:predicates
+		(secret)
+		(secret_at ?r - room)
+		(at ?r - room)
+		(succ ?nr ?r - room)
+	)
+
+	(:functions
+		(a_loc) - loc
+		(fact ?p - query) - boolean
+		
+		;(check ?r - loc ?q - query) - boolean
+		(@check ?r - loc ?q - query ?r - loc ?r - loc ?r - loc) - boolean
+	)
+
+	;this is where to put all negative stuff
+	; (:derived (enable_nega)
+ ;        (forall (?n - neg) (not (= (fact ?n) 1)))
+ ;    )
+
+	(:action shout
+		:parameters(?r - room)
+		:precondition
+		(and
+			(secret)
+			(at ?r)
+
+		)
+		:effect
+		(and
+			;(forall (?q - query) (when (= (check (a_loc) ?q) 1) (assign (fact ?q) 1)))
+			(forall (?q - query) (when (= (@check (a_loc) ?q (a_loc) (a_loc) (a_loc)) 1) (assign (fact ?q) 1)))
+		)
+	)
+
+	(:action sense
+		:parameters(?r - room)
+		:precondition
+		(and
+			(at ?r)
+			(secret_at ?r)
+		)
+		:effect
+		(and
+			(secret)
+		)
+	)
+
+	(:action move_right
+		:parameters(?r ?nr - room)
+		:precondition
+		(and
+			(at ?r)
+			(succ ?r ?nr)
+		)
+		:effect
+		(and
+			(not (at ?r))
+			(at ?nr)
+			(assign (a_loc) (+ (a_loc) 1))
+		)
+	)
+
+	(:action move_left
+		:parameters(?r ?nr - room)
+		:precondition
+		(and
+			(at ?r)
+			(succ ?nr ?r)
+		)
+		:effect
+		(and
+			(not (at ?r))
+			(at ?nr)
+			(assign (a_loc) (- (a_loc) 1))
+		)
+	)
+
+	; (:action dummy_action
+	; 		:parameters()
+	; 		:precondition
+	; 		(and 
+	; 			(false)
+	; 			)
+	; 		:effect
+	; 		(and
+	; 			(not (enable_nega))
+	; 		)
+	; )
+
+)
